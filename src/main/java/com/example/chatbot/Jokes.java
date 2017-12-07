6b2ecc82d7f25c79e98ac5fc20d40f5027ec875b
Matipolit <matipolit@gmail.com>
2017-12-07 16:48:21 +0100

    Final translation of jokes

--- src/main/java/com/example/chatbot/Jokes.java
+++ src/main/java/com/example/chatbot/Jokes.java
@@ -15,9 +15,9 @@ public class Jokes {
     Jokes() {
         jokes = new ArrayList<>();
 
-        jokes.add("Pierwotnie napisałem tę funkcję getQueryParameters, aby zwracała ciąg kwerendy paska adresu jako obiekt klucza / wartości, pierwotnie był jak dwadzieścia-liniowa funkcja z kondycjonowaniem i sprawdzaniem błędów i gówno w tym stylu. Myślałem, że mogę to zrobić lepiej.");
+        jokes.add("Pierwotnie napisałem tę funkcję getQueryParameters, aby zwracała ciąg kwerendy paska adresu jako obiekt klucza / wartości, pierwotnie była to ok. dwudziesto-liniowa funkcja z warunkami i sprawdzaniem błędów i gównem w tym stylu. Myślałem, że mogę to zrobić lepiej.");
         jokes.add("Komputer raz pokonał mnie w szachach, ale nie miał ze mną szans w kick boxingu.");
         jokes.add("Dopóki istnieją testy, w szkołach będzie modlitwa. ");
-        jokes.add("What did one ocean say to the other ocean? Nothing, they just waved.");
+        jokes.add("Co powiedział ocean do oceanu? Nic, tylko pomachał.");
     }
 }
